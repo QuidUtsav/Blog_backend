a1a65152479d3b83b9d4c8f2038729a02f401fed
QuidUtsav~ <pas081bct048@wrc.edu.np>
2026-07-02 22:34:37 +0545

    access tokens and checking jwt from user login

--- auth.py
+++ auth.py
@@ -1,7 +1,10 @@
-import bcrypt
-from jose import jwt
-from datetime import datetime, timedelta
 import os
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError, jwt
+import bcrypt
+from datetime import datetime, timedelta
+
 
 def hash_password(password: str) -> str:
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
@@ -12,8 +15,21 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = "HS256"
+
 def create_access_token(data: dict, expires_minutes: int = 30):
     to_encode = data.copy()
     expire = datetime.now() + timedelta(minutes=expires_minutes)
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+def get_current_account(token: str = Depends(oauth2_scheme)):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        account_id: str = payload.get("sub")
+        if account_id is None:
+            raise HTTPException(status_code=401, detail="Invalid token")
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Invalid token")
+    return account_id
